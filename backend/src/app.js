f5cb771e6619b2320c6db1e5e5fc6896bd8cc1c9
Mohamed Ibrahim <muhamedibrahim25@gmail.com>
2022-12-16 22:34:04 +0200

    update cors origin as env var

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_ORIGIN,
   })
 );
 app.use(express.json());
